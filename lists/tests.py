733f99df4ee261a0f02d1daf25502e8a356af85b
alivia.khaira <alivia.khaira@ui.ac.id>
2015-09-10 04:23:43 +0000

    test with django render

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import home_page
 
 # Create your tests here.
@@ -13,8 +14,8 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
 		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>Alivia Khaira 1206277464</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
-
-
+		self.assertIn(b'<title>To-Do Lists</title>', response.content)
+		self.assertTrue(response.content.strip().endswith(b'</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>To-Do Lists</title><body><h1>To-Do Lists for today </h1></body></html>')
+	return render(request, 'home.html')
