88cfd001aead07cd07d6e874d26b59b2d61bff6e
alivia.khaira <alivia.khaira@ui.ac.id>
2015-09-20 12:51:44 +0000

    Tambah pmpl_list_url dll

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -29,6 +29,9 @@ class NewVisitorTest(LiveServerTestCase):
 
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
+
+		pmpl_list_url = self.browser.current_url
+		self.assertRegex(pmpl_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -37,6 +40,25 @@ class NewVisitorTest(LiveServerTestCase):
 
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		pmpl2_list_url = self.browser.current_url
+		self.assertRegex(pmpl2_list_url, '/lists/.+')
+		self.assertNotEqual(pmpl2_list_url, pmpl_list_url)
+		
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
