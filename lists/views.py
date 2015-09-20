1b604834fb6b3110b62ba30f31ed63ea12132a54
alivia.khaira <alivia.khaira@ui.ac.id>
2015-09-20 14:54:37 +0000

    new url, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -4,17 +4,20 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+	#pass
 	if request.method == 'POST':
 		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
-	
+		return redirect('/lists/the-only-list-in-the-world/')
+	return render(request, 'home.html')	
+
+def view_list(request):
 	items = Item.objects.all()
 	itempribadi =''
-	
+
 	if Item.objects.count()==0 :
 		itempribadi = 'yey, waktunya berlibur'
 	elif Item.objects.count()<5 :
 		itempribadi = 'sibuk tapi santai'
 	elif Item.objects.count()>=5 :
 		itempribadi = 'oh tidak'
-	return render(request, 'home.html', {'items': items, 'itempribadi': itempribadi})
+	return render(request, 'list.html', {'items': items, 'itempribadi': itempribadi})
--- superlists/urls.py
+++ superlists/urls.py
@@ -4,6 +4,7 @@ from lists import views
 
 urlpatterns = [
 	url(r'^$', views.home_page, name='home'),
+	url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
