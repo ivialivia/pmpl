94d95098bbe02241a911c226b9d2e5d08d19d758
alivia.khaira <alivia.khaira@ui.ac.id>
2015-09-20 12:09:07 +0000

    Commit stlh clone github

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,20 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	return render(request, 'home.html', {
-		'new_item_text': request.POST.get('item_text',''),
-	})
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/')
+	
+	items = Item.objects.all()
+	itempribadi =''
+	
+	if Item.objects.count()==0 :
+		itempribadi = 'yey, waktunya berlibur'
+	elif Item.objects.count()<5 :
+		itempribadi = 'sibuk tapi santai'
+	elif Item.objects.count()>=5 :
+		itempribadi = 'oh tidak'
+	return render(request, 'home.html', {'items': items, 'itempribadi': itempribadi})
